e464b284372571440b47c9a5b1ae3198f7badb51
Skywalger <skywalger@t-online.de>
2016-06-11 10:55:35 +0200

    TEst1

---
+++ TEstTest/src/testtest/TEstTest.java
@@ -0,0 +1,13 @@
+package testtest;
+
+/**
+ * @author Sky aka. David Leister
+ */
+public class TEstTest {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Das ist ein \"GitTest\"");
+    }
+
+}
